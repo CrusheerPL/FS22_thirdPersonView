77f42bc1c5c1f10901318c9b3669b910c81dbdf7
CrusheerPL <35576727+CrusheerPL@users.noreply.github.com>
2021-12-27 19:58:56 +0100

    Update 0.1.1
    PL:
    - widok postaci nie przełącza się po wyjściu z pojazdu (przy załączonym widoku z trzeciej osoby)
    - widok z trzeciej osoby nie buguje się po wyjściu z pojazdu lub z menu konfiguracji postaci
    - skompletowano tłumaczenia do przypisań klawiszy
    
    EN:
    - Player view isn't switching after leaving a vehicle (when 3rd person view enabled)
    - 3rd person view isn't bugged after leaving the vehicle or the wardrobe menu
    - completed l10n for key bindings
    
    *work in progress*

--- main.lua
+++ main.lua
@@ -1,10 +1,30 @@
 source(g_currentModDirectory.."thirdPersonView.lua")
+local tpv
 
-local function playernew_inj(self, superFunc, isServer, isClient)
+function init()
+    FSBaseMission.delete = Utils.appendedFunction(FSBaseMission.delete, unload)
+    Mission00.load = Utils.prependedFunction(Mission00.load, mload)
+	
+	Player.new = Utils.overwrittenFunction(Player.new, playernew_inj)
+end
+
+-- nie wiem co daja te dwie funkcje ale niech beda
+function mload(mission)
+	tpv = ThirdPersonView:new()
+	mission.tpv = tpv
+	addModEventListener(tpv)
+end
+
+function unload()
+	removeModEventListener(tpv)
+	tpv:delete()
+	tpv = nil
+end
+
+function playernew_inj(self, superFunc, isServer, isClient)
 	f = superFunc(self, isServer, isClient)
 	f.inputInformation.registrationList[InputAction.TOGGLE_3RD_PERSON_VIEW] = {callback=ThirdPersonView.toggleThirdPersonView, triggerUp=false, triggerDown=true, triggerAlways=false, activeType=Player.INPUT_ACTIVE_TYPE.STARTS_ENABLED, callbackState=nil, text=g_i18n:getText("input_TOGGLE_3RD_PERSON_VIEW"), textVisibility=true}
 	return f
 end
 
-Player.new = Utils.overwrittenFunction(Player.new, playernew_inj)
-Player.onLeaveVehicle = Utils.appendedFunction(Player.onLeaveVehicle, ThirdPersonView.onLeaveVehicle_inj)
+init()
--- thirdPersonView.lua
+++ thirdPersonView.lua
@@ -2,19 +2,18 @@
 local ThirdPersonView_mt = Class(ThirdPersonView)
 
 function ThirdPersonView.new()
-	local self = Object.new(ThirdPersonView_mt)
+	local self = {}
+	setmetatable({}, ThirdPersonView_mt)
 
 	self.viewState = false -- is player 3rd person view enabled?
+    Player.onLeaveVehicle = Utils.appendedFunction(Player.onLeaveVehicle, ThirdPersonView.restoreView)
+    Player.onEnter = Utils.appendedFunction(Player.onEnter, ThirdPersonView.restoreView)
 
 	return self
 end
 
-function ThirdPersonView:onEnterVehicle()
-	if self.viewState then g_currentMission.player:consoleCommandThirdPersonView() end
-end
-
-function ThirdPersonView:onLeaveVehicle_inj()
-	if self.viewState then g_currentMission.player:consoleCommandThirdPersonView() end
+function ThirdPersonView:restoreView()
+	if self.viewState then for i=1, 2 do g_currentMission.player:consoleCommandThirdPersonView() end end
 end
 
 function ThirdPersonView:toggleThirdPersonView()
